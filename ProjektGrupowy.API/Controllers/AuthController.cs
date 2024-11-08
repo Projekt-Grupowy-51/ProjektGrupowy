a18eee95667e8a9db7f7423aa766e84289b74471
witczakxd <kacperwitczak2003@icloud.com>
2024-11-08 23:05:22 +0100

    Added auth to project

---
+++ ProjektGrupowy.API/Controllers/AuthController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ProjektGrupowy.API.Data;
+using ProjektGrupowy.API.DTOs.Auth;
+using ProjektGrupowy.API.Filters;
+using ProjektGrupowy.API.Models;
+using ProjektGrupowy.API.Utils;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProjektGrupowy.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[ServiceFilter(typeof(ValidateModelStateFilter))] // ValidateModelStateFilter is a custom filter to validate model state
+public class AuthController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager,
+                                AppDbContext context, IConfiguration configuration ) : ControllerBase
+{
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto model)
+    {
+        var userExists = await userManager.FindByNameAsync(model.UserName);
+        if (userExists != null)
+            return StatusCode(500, new { Status = "Error", Message = "User already exists!" });
+
+        var user = new User
+        {
+            UserName = model.UserName,
+            Email = model.Email,
+            SecurityStamp = Guid.NewGuid().ToString()
+        };
+
+        using (var transaction = await context.Database.BeginTransactionAsync())
+        {
+            try
+            {
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (!result.Succeeded)
+                {
+                    var errors = result.Errors.Select(e => e.Description).ToList();
+                    return StatusCode(500, new { Status = "Error", Message = "User creation failed!", Errors = errors });
+                }
+
+                if (!await roleManager.RoleExistsAsync(model.Role.ToString()))
+                    return StatusCode(500, new { Status = "Error", Message = "Role does not exist!" });
+
+                var roleResult = await userManager.AddToRoleAsync(user, model.Role.ToString());
+                if (!roleResult.Succeeded)
+                    return StatusCode(500, new { Status = "Error", Message = "Failed to assign role to user." });
+
+                await transaction.CommitAsync();
+
+                return Ok(new { Status = "Success", Message = "User created and assigned to role successfully!" });
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                return StatusCode(500, new { Status = "Error", Message = "An error occurred during registration.", Detail = ex.Message });
+            }
+        }
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto model)
+    {
+        var user = await userManager.FindByNameAsync(model.UserName);
+        if (user == null || !await userManager.CheckPasswordAsync(user, model.Password))
+            return Unauthorized(new { Status = "Error", Message = "Invalid UserName or password!" });
+
+        var authClaims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        };
+
+        var userRoles = await userManager.GetRolesAsync(user);
+        foreach (var userRole in userRoles)
+        {
+            authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+        }
+
+        var token = GenerateJwtToken(authClaims);
+
+        return Ok(new
+        {
+            Token = new JwtSecurityTokenHandler().WriteToken(token),
+            Expiration = token.ValidTo
+        });
+    }
+
+    private JwtSecurityToken GenerateJwtToken(IEnumerable<Claim> authClaims)
+    {
+        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+
+        var token = new JwtSecurityToken(
+            issuer: configuration["JWT:ValidIssuer"],
+            audience: configuration["JWT:ValidAudience"],
+            expires: DateTime.Now.AddHours(3),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return token;
+    }
+}
--- ProjektGrupowy.API/Program.cs
+++ ProjektGrupowy.API/Program.cs
@@ -1,18 +1,27 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ProjektGrupowy.API.Data;
 using ProjektGrupowy.API.DB;
+using ProjektGrupowy.API.Filters;
 using ProjektGrupowy.API.Mapper;
+using ProjektGrupowy.API.Models;
 using ProjektGrupowy.API.Repositories;
 using ProjektGrupowy.API.Repositories.Impl;
 using ProjektGrupowy.API.Services;
 using ProjektGrupowy.API.Services.Impl;
+using ProjektGrupowy.API.Utils.Enums;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Log Configuration
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
-    .WriteTo.Console() // Ensure this line now works
+    .WriteTo.Console()
     .WriteTo.File("Logs/internal_api.log", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
@@ -23,7 +32,9 @@ builder.Host.UseSerilog();
 
 var app = builder.Build();
 
-//await SeedDatabase(app.Services);
+// Seed the database and create roles
+await SeedDatabase(app.Services);
+await CreateRoles(app.Services);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -34,12 +45,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-return;
 
 static void AddServices(WebApplicationBuilder builder)
 {
@@ -49,11 +60,34 @@ static void AddServices(WebApplicationBuilder builder)
 
     builder.Services.AddScoped<IProjectService, ProjectService>();
     builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-
     builder.Services.AddAutoMapper(typeof(ProjectMap));
+    builder.Services.AddScoped<ValidateModelStateFilter>();
 
     builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddIdentity<User, IdentityRole>()
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders();
+
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
 }
 
 static async Task SeedDatabase(IServiceProvider serviceProvider)
@@ -62,3 +96,35 @@ static async Task SeedDatabase(IServiceProvider serviceProvider)
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     await DbSeeder.SeedAsync(context);
 }
+
+static async Task CreateRoles(IServiceProvider serviceProvider)
+{
+    using var scope = serviceProvider.CreateScope();
+
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    IdentityResult roleResult;
+
+    var roleNames = Enum.GetValues(typeof(RoleEnum)).Cast<RoleEnum>();
+
+    foreach (var roleName in roleNames)
+    {
+        var roleExist = await roleManager.RoleExistsAsync(roleName.ToString());
+        if (!roleExist)
+        {
+            roleResult = await roleManager.CreateAsync(new IdentityRole(roleName.ToString()));
+            if (roleResult.Succeeded)
+            {
+                Log.Information($"Rola {roleName} zosta³a pomyœlnie utworzona.");
+            }
+            else
+            {
+                Log.Error($"B³¹d przy tworzeniu roli {roleName}: {string.Join(", ", roleResult.Errors.Select(e => e.Description))}");
+            }
+        }
+        else
+        {
+            Log.Information($"Rola {roleName} ju¿ istnieje.");
+        }
+    }
+}
+
