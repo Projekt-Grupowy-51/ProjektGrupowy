a408955ca2a1a5f1eb8f03f81fbec7902ffde991
Henryk Wolek <henrykwolek15@gmail.com>
2025-03-24 21:30:38 +0100

    Added video batches to frontend

--- ProjektGrupowy.API/DTOs/VideoGroup/VideoGroupResponse.cs
+++ ProjektGrupowy.API/DTOs/VideoGroup/VideoGroupResponse.cs
@@ -5,5 +5,6 @@ public class VideoGroupResponse
     public int Id { get; set; }
     public string Name { get; set; }
     public int ProjectId { get; set; }
-    public int VideoCount { get; set; }
+    // public int VideoCount { get; set; }
+    public Dictionary<int, int> VideosAtPositions { get; set; }
 }
