abab487c9705d3924e8b830fab0af330f0f315d8
kacpe <kacpe@LAPTOP_KACPER>
2025-10-18 17:17:33 +0200

    project structure cleanup

--- frontend-new/vite.config.js
+++ frontend-new/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 3000, // React dzia³a na porcie 3000
+        port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:5000', // Backend lokalny
+                target: 'http://localhost:5000',
                 changeOrigin: true,
             },
         },
