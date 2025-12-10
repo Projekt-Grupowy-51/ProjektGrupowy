2b68efb6e7de2ef47470e27cd6cf77a7ebd02581
kacpe <kacpe@LAPTOP_KACPER>
2025-12-10 21:14:21 +0100

    fix

--- VidMark.API/Mapper/ProjectMap.cs
+++ VidMark.API/Mapper/ProjectMap.cs
@@ -17,5 +17,12 @@ public class ProjectMap : Profile
             .ForMember(dest => dest.ModificationDate, opt => opt.MapFrom(src => src.ModificationDate))
             .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => src.EndDate))
             .ForMember(dest => dest.Finished, opt => opt.MapFrom(src => src.EndDate.HasValue));
+
+        CreateMap<Project, ProjectStatsResponse>()
+            .ForMember(dest => dest.Subjects, opt => opt.MapFrom(src => src.Subjects.Count))
+            .ForMember(dest => dest.Videos, opt => opt.MapFrom(src => src.VideoGroups.Count))
+            .ForMember(dest => dest.Assignments, opt => opt.MapFrom(src => src.Subjects.SelectMany(s => s.SubjectVideoGroupAssignments).DistinctBy(x => x.Id).Count()))
+            .ForMember(dest => dest.Labelers, opt => opt.MapFrom(src => src.ProjectLabelers.Count))
+            .ForMember(dest => dest.AccessCodes, opt => opt.MapFrom(src => src.AccessCodes.Count));
     }
 }
